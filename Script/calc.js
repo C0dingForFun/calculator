ac02ea6e9abc9481e9259bef7f6c55e29d019f37
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-05-17 13:27:07 +0200

    yoh

--- Script/calc.js
+++ Script/calc.js
@@ -1,8 +1,27 @@
-let buttons = document.getElementsByTagName('button');
-let input = document.getElementsByTagName('input');
+
+let btnDivide = document.querySelector('[value="/"')
+let btnMultiply = document.querySelector('[value="*"')
+let btnMinus = document.querySelector('[value="-"')
+let btnPlus = document.querySelector('[value="+"')
+let btnSquare = document.querySelector('[value="**"')
+let btnModulus = document.querySelector('[value="%"')
+let btnAc = document.querySelector('[value="AC"')
+let buttons = document.getElementsByTagName('button')
+
+let input = document.querySelector('input')
+
+let answer = document.querySelector('#equal')
 
 for (let button of buttons) {
     button.addEventListener('click',(event)=>{
-        console.log(event.target.innerText);
+        input.value +=  event.target.value;
     })
 }
+
+btnAc.addEventListener('click', ()=>{
+    input.value = "";
+})
+
+answer.addEventListener('click', ()=>{
+    input.value = eval(`${input.value}`);
+})
